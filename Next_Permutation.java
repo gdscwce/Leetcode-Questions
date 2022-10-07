cc2952bb795f0926d9948132c8e1054d604af8d9
Rohit G <112975781+rohitgandeLeetcode@users.noreply.github.com>
2022-10-08 00:33:43 +0530

    Create Next_Permutation.java

---
+++ Next_Permutation.java
@@ -0,0 +1,39 @@
+// A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+// Medium
+// problem number -31
+// link = https://leetcode.com/problems/next-permutation/
+
+
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        
+    int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
