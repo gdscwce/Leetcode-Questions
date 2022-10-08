828f90a4d5e1fced6deb307b0b70b4cac50d49f2
Durvesh N. Patil <86490574+durveshnp@users.noreply.github.com>
2022-10-08 11:06:44 +0530

    Update 1209-remove-all-adjacent-duplicates-in-string-ii.cpp

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        int i, j;
+        stack<int> stk;
+        stk.push(0);
+        for (i = 1, j = 1; j < s.size(); i++, j++) 
+        {
+            s[i] = s[j];
+            if (i == 0 || s[i] != s[i-1]) 
+                stk.push(i);
+            else if (i - stk.top() + 1 == k) 
+            {
+                i = stk.top() - 1;
+                stk.pop();
+            }
+        }
+        return s.substr(0, i);
+    }
+};
